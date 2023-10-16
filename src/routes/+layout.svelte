f6258917039ec11393912aa0864fba278968dc2d
laureningy <78576502+laureningy@users.noreply.github.com>
2023-10-16 11:47:28 -0400

    Basic navigation setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang='ts'>
+    const navItems = [
+        {label: "Home", a: "/"},
+        {label: "Concept Designs", a:"/ConceptDesigns"},
+        {label: "Our Team", a:"/OurTeam"}
+    ];
+</script>
+
+<style>
+    nav{
+        height: 45px;
+
+    }
+    a{
+        color: #fff;
+        text-decoration: none;
+        display: float;
+        height: 45px;
+        align-items: center;
+        padding: 0 10px;
+        font-size: 16px;
+    }
+</style>
+
+<nav>
+    {#each navItems as item}
+        <a href={item.a}>{item.label}</a>
+    {/each}
+</nav>
+<slot></slot>
