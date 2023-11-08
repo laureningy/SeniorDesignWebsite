e4ef43c5eadc52800304f2f5cdacc40863adc5e2
laureningy <78576502+laureningy@users.noreply.github.com>
2023-11-08 12:54:39 -0500

    ????

--- src/routes/Team/+page.svelte
+++ src/routes/Team/+page.svelte
@@ -1,63 +1,61 @@
 <script>
   import TeamList from './TeamList.svelte';
 
-  const isVercel = import.meta.env.VITE_VERCEL_ENV === 'true'; // Check if running on Vercel
-
   let team = [
     {
       id: 1,
       name: 'Eshan Sharma',
       role: 'Software Engineer',
       bio: '',
-      imageSrc: '',
+      imageSrc: '/images/EshanPhoto.jpg', 
     },
     {
       id: 2,
       name: 'Lauren Ingraham',
       role: 'Software Engineer',
       bio: '',
-      imageSrc: isVercel ? '/images/LaurenPhoto.jpg' : '../../public/images/LaurenPhoto.jpg',
+      imageSrc: '/LaurenPhoto.jpg',
     },
     {
       id: 3,
       name: 'Colin Keady',
       role: 'Mechanical Engineer',
       bio: '',
-      imageSrc: isVercel ? '/images/ColinPhoto.jpeg' : '../../public/images/ColinPhoto.jpeg',
+      imageSrc: './images/ColinPhoto.jpeg',
     },
     {
       id: 4,
       name: 'Michael Goodall',
       role: 'Mechanical Engineer',
       bio: '',
-      imageSrc: isVercel ? '/images/MikePhoto.jpg' : '../../public/images/MikePhoto.jpg',
+      imageSrc: '/images/MikePhoto.jpg',
     },
     {
       id: 5,
       name: 'Nicholas Dahdah',
       role: 'Mechanical Engineer',
       bio: '',
-      imageSrc: isVercel ? '/images/NickPhoto.jpg' : '../../public/images/NickPhoto.jpg',
+      imageSrc: '/images/NickPhoto.jpg',
     },
     {
       id: 6,
       name: 'Nicole Waletzki',
       role: 'Mechanical Engineer',
       bio: '',
-      imageSrc: isVercel ? '/images/NicolePhoto.jpeg' : '../../public/images/NicolePhoto.jpeg',
+      imageSrc: '/images/NicolePhoto.jpeg',
     },
   ];
 </script>
 
-  
-  <main>
-    <h1>Meet the Team</h1>
-    <TeamList {team} />
-  </main>
-  
-  <style>
-     h1{
-        font-family: 'Oswald Variable', sans-serif;
-        font-weight: 500;
-    }
-  </style>
+<main>
+  <h1>Meet the Team</h1>
+  <TeamList {team} />
+</main>
+
+<style>
+  h1 {
+    font-family: 'Oswald Variable', sans-serif;
+    font-weight: 500;
+  }
+  /* You can add more styles here as needed */
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
-  server: {
-    // Add a custom Vite server configuration
-    fs: {
-      // Allow serving files from the "public" directory
-      allow: ['..'], // This allows serving files outside of the "src" directory
-    },
-  },
 });
