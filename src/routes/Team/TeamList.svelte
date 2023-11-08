f06ecef47d5af3039f9405c99e6c5ad2c6ab4e8e
laureningy <78576502+laureningy@users.noreply.github.com>
2023-11-07 22:43:58 -0500

    photo fix

--- src/routes/Team/TeamList.svelte
+++ src/routes/Team/TeamList.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import TeamMember from './TeamMember.svelte';
   
     export let team;
--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    // Add a custom Vite server configuration
+    fs: {
+      // Allow serving files from the "public" directory
+      allow: ['..'], // This allows serving files outside of the "src" directory
+    },
+  },
 });
